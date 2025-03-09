99abc90fe707685766a5c195bad4cc59b1e41808
XuanNam <dangxuannam58@gmail.com>
2025-03-09 14:20:08 +0700

    update

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import { IoReorderThreeOutline } from "react-icons/io5";
 import { menu } from "./SidebarConfig";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const [activeTab, setActiveTab] = useState();
-const navigate = useNavigate();
+  const { user } = useSelector((store) => store);
+  const navigate = useNavigate();
 
-  const handleTabClick=(title) =>{
+  const handleTabClick = (title) => {
     setActiveTab(title);
     if (title === "Profile") {
-      navigate("/username");
-    }
-    else if (title === "Home") {
+      navigate(`/${user.reqUser.username}`);
+    } else if (title === "Home") {
       navigate("/");
     }
-  }
+  };
   return (
     <div className="sticky top-0 h-[100vh]">
       <div className="flex flex-col justify-between h-full px-10">
@@ -31,19 +32,27 @@ const navigate = useNavigate();
 
           <div className="mt-10">
             {menu.map((item) => (
-              <div onClick={()=>handleTabClick(item.title)} className="flex items-center mb-5 cursor-pointer text-lg">
+              <div
+                onClick={() => handleTabClick(item.title)}
+                className="flex items-center mb-5 cursor-pointer text-lg"
+              >
                 {activeTab === item.title ? item.activeIcon : item.icon}
-                <p className={`${activeTab === item.title ? "font-bold" : "font-semibold"}`}>{item.title}</p>
+                <p
+                  className={`${
+                    activeTab === item.title ? "font-bold" : "font-semibold"
+                  }`}
+                >
+                  {item.title}
+                </p>
               </div>
             ))}
           </div>
         </div>
 
         <div className="flex items-center cursor-pointer pb-10">
-        <IoReorderThreeOutline className="text-2xl" />
-        <p className="ml-5">More</p>
-      </div>
-
+          <IoReorderThreeOutline className="text-2xl" />
+          <p className="ml-5">More</p>
+        </div>
       </div>
     </div>
   );
